66656143d776252926d2cc342e104374a18cf48b
Nguyễn Khắc Qui <hivongmotloihua@gmail.com>
2021-02-23 12:02:19 +0700

    them role voi seller

--- src/main/java/com/codegym/model/Role.java
+++ src/main/java/com/codegym/model/Role.java
@@ -1,4 +1,40 @@
 package com.codegym.model;
 
-public class Role {
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Set;
+
+@Entity
+@Table(name = "role")
+public class Role implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column("role_id")
+    private Long id;
+
+    @Column(name = "name_role")
+    private String roleName;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<Seller> sellers;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
 }
